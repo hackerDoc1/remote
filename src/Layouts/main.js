f8eeb848d5f6a4118e923e75035186f02ae60ad1
Shaliko <h.shaliko@mail.ru>
2021-09-13 23:10:37 +0400

    d

--- src/Layouts/main.js
+++ src/Layouts/main.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Header } from "../Components/Header/header";
-import { MainAdversting } from "../Components/MainAdversting/mainAdversting";
-import { Footer } from "../Components/Footer/footer";
 import { StepContainer } from "../StepChnager/StepContainer/stepContainer";
 
 import "./layout.css";
